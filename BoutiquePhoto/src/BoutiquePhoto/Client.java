40831c6ea6cbbeabd1f5fa5141dec32c6c351e1a
Likton <44likton44@gmail.com>
2016-10-03 17:31:30 +0200

    MAJ classes: Client, Magasin -> accesseurs

--- BoutiquePhoto/src/BoutiquePhoto/Client.java
+++ BoutiquePhoto/src/BoutiquePhoto/Client.java
@@ -3,4 +3,12 @@ package BoutiquePhoto;
 public class Client {
 
 	private String sNom;
+
+	public String getsNom() {
+		return sNom;
+	}
+
+	public void setsNom(String sNom) {
+		this.sNom = sNom;
+	}
 }
--- BoutiquePhoto/src/BoutiquePhoto/Magasin.java
+++ BoutiquePhoto/src/BoutiquePhoto/Magasin.java
@@ -4,4 +4,17 @@ public class Magasin {
 
 	private String sNom;
 	private String sNomGerant;
+	
+	public String getsNom() {
+		return sNom;
+	}
+	public void setsNom(String sNom) {
+		this.sNom = sNom;
+	}
+	public String getsNomGerant() {
+		return sNomGerant;
+	}
+	public void setsNomGerant(String sNomGerant) {
+		this.sNomGerant = sNomGerant;
+	}
 }
