34586798abe316ba1fbcaa0979f9551182c95ac3
Shadow76123 <matthieu.chardenal@gmail.com>
2016-10-04 11:14:30 +0200

    MAJ class Magasin => traitement AfficherLocationEnCours()

--- BoutiquePhoto/src/BoutiquePhoto/Magasin.java
+++ BoutiquePhoto/src/BoutiquePhoto/Magasin.java
@@ -27,10 +27,20 @@ public class Magasin {
 	}
 	
 	//méthodes
-	
-	public void AfficherLocationsEnCours() 
+	/*
+	 * Permet d'afficher la liste des articles loué par un client passé en parametre
+	 */
+	public void AfficherLocationsEnCours(Client pClient) 
 	{
-		
+		System.out.println("Liste des locations du client : "+pClient.getsNom());
+		for(Location currentLocation : pClient.getlLocations()) {
+			if(currentLocation.getDateFin() == null) {
+				System.out.println("Location du : "+currentLocation.getDateDebut());
+				for(Article currentArticle : currentLocation.getlArticles()) {
+					System.out.println("N° Ref : "+currentArticle.getnReference()+" | Nom : "+currentArticle.getsIntitule());
+				}
+			}
+		}
 	}
 	
 	public int CalculerMontant()
