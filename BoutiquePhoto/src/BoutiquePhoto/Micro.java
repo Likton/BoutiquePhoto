323eeee443f991bb6059c74dbfb0a2544cf07ae5
Shadow76123 <matthieu.chardenal@gmail.com>
2016-10-04 08:29:08 +0200

    MAJ Class Micro => Ajout attributs/accesseurs/constructeur MAJ Class Objectif => Ajout attributs/accesseurs/constructeur

--- BoutiquePhoto/src/BoutiquePhoto/Micro.java
+++ BoutiquePhoto/src/BoutiquePhoto/Micro.java
@@ -1,5 +1,45 @@
 package BoutiquePhoto;
 
-public class Micro extends Article{
+public class Micro extends Article {
+
+	// Attributs
+
+	private double dSensibilite;
+	private boolean bTypeFilaire;
+	private double dAutonomie;
+
+	// Accesseurs
+	public double getdSensibilite() {
+		return dSensibilite;
+	}
+
+	public void setdSensibilite(double dSensibilite) {
+		this.dSensibilite = dSensibilite;
+	}
+
+	public boolean isbTypeFilaire() {
+		return bTypeFilaire;
+	}
+
+	public void setbTypeFilaire(boolean bTypeFilaire) {
+		this.bTypeFilaire = bTypeFilaire;
+	}
+
+	public double getdAutonomie() {
+		return dAutonomie;
+	}
+
+	public void setdAutonomie(double dAutonomie) {
+		this.dAutonomie = dAutonomie;
+	}
+	
+	// Constructeur
+	
+	public Micro(double pSensibilite, boolean pTypeFilaire, double pAutomonie) {
+		super();
+		this.dSensibilite = pSensibilite;
+		this.bTypeFilaire = pTypeFilaire;
+		this.dAutonomie = pAutomonie;
+	}
 
 }
--- BoutiquePhoto/src/BoutiquePhoto/Objectif.java
+++ BoutiquePhoto/src/BoutiquePhoto/Objectif.java
@@ -1,5 +1,27 @@
 package BoutiquePhoto;
 
+import java.util.TreeSet;
+
 public class Objectif {
 
+	// Attribut
+	
+	private TreeSet<String> lAppPhoto;
+	
+	// Accesseurs
+	
+	public TreeSet<String> getlAppPhoto() {
+		return lAppPhoto;
+	}
+
+	public void setlAppPhoto(TreeSet<String> lAppPhoto) {
+		this.lAppPhoto = lAppPhoto;
+	}
+	
+	// Constructeur
+	
+	public Objectif(TreeSet<String> pLappPhoto) {
+		super();
+		this.lAppPhoto = pLappPhoto;
+	}
 }
