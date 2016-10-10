678f72949dd6308b951fb59430db5fafeb1b7edd
Shadow76123 <matthieu.chardenal@gmail.com>
2016-10-10 09:47:25 +0200

    Test commit

--- BoutiquePhoto/src/BoutiquePhoto/ArticleMarqueComparator.java
+++ BoutiquePhoto/src/BoutiquePhoto/ArticleMarqueComparator.java
@@ -5,6 +5,12 @@ import java.util.Comparator;
 public class ArticleMarqueComparator implements Comparator<Article> {
 
 	public int compare(Article pArticle1, Article pArticle2) {
-		return pArticle1.getsMarque().equalsIgnoreCase(pArticle2.getsMarque());
+		int result = 0;
+		if(!(pArticle1.getsMarque().equalsIgnoreCase(pArticle2.getsMarque()))) {
+			result = 1;
+		} else {
+			result = -1;
+		}
+		return result;
 	}
 }
